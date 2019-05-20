b423ae52f7682002c383de9e438b52d19cfbf584
schkiho <schkiho@yahoo.de>
2019-05-20 13:46:42 +0200

    added build

---
+++ build/precache-manifest.265dd4634dc0515701473c46d075182a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "f0d6e164c2bc5625977b",
+    "url": "/static/css/main.ef84a326.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "2cfd3883aadf333b509b",
+    "url": "/static/js/2.2cfd3883.chunk.js"
+  },
+  {
+    "revision": "2c706bd7f8fd065efa5dff5eb1ac099e",
+    "url": "/static/media/logo.2c706bd7.png"
+  },
+  {
+    "revision": "f0d6e164c2bc5625977b",
+    "url": "/static/js/main.f0d6e164.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "2cfd3883aadf333b509b",
+    "url": "/static/css/2.938aeb50.chunk.css"
+  },
+  {
+    "revision": "76634dd33e008cd67c0e0c2721315b45",
+    "url": "/index.html"
+  }
+];
